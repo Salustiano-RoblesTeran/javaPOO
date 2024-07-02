b594cb054feac45b341e919689cdffb99c773dea
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-07-01 21:28:25 -0300

    practica clases abstractas

---
+++ javaPOO/src/main/java/com/salu/javapoo/Figura.java
@@ -0,0 +1,12 @@
+package com.salu.javapoo;
+
+
+public abstract class Figura {
+    
+    
+    protected double x; // Pos en x
+    protected double y; // Pos en y
+    
+    public abstract double calcularArea();
+    
+}
